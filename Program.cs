cbc9ff3c2c25cf597187715fcc1e6ccbe349f706
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-03-20 11:01:03 +0700

    implementasi-generic-class

--- Program.cs
+++ Program.cs
@@ -1,2 +1,39 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+public class Program
+{
+    public class SimpleDataBase<T>
+    {
+        private List<T>StoredDataBase;
+        private List<DateTime> inputDates;
+
+        public SimpleDataBase()
+        {
+            StoredDataBase = new List<T>();
+            inputDates = new List<DateTime>();
+        }
+
+        public void AddNewData(T data)
+        {
+            StoredDataBase.Add(data);
+            inputDates.Add(DateTime.Now);
+        }
+
+        public void PrintAllData()
+        {
+            for (int i = 0; i < StoredDataBase.Count; i++)
+            {
+                Console.WriteLine("Data: " + (i+1)  + "berisi: " + StoredDataBase[i] + " yang disimpan pada waktu UTC: " + inputDates[i]);
+            }
+        }
+
+    }
+    public static void Main(string[] args)
+    {
+        SimpleDataBase<int> db = new SimpleDataBase<int>();
+        db.AddNewData(10);
+        db.AddNewData(30);
+        db.AddNewData(22);
+        db.PrintAllData();
+    }
+}
